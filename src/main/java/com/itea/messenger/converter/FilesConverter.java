72bb19a3614445e144f0676b8c96ab0213806832
KomSoft <edshiyan73@gmail.com>
2022-08-31 20:22:35 +0300

    1. Refactoring.

--- src/main/java/com/itea/messenger/converter/FilesConverter.java
+++ src/main/java/com/itea/messenger/converter/FilesConverter.java
@@ -8,18 +8,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class FilesConverter {
     public Files fileEntityFromDto(FilesDto filesDto){
-        Files files = new Files();
-        files.setId(filesDto.getId());
-        files.setFileType(filesDto.getFileType());
-        files.setFile(filesDto.getFile());
-        return files;
+        Files file = new Files();
+        file.setId(filesDto.getId());
+        file.setFileType(filesDto.getFileType());
+        file.setFileName(filesDto.getFileName());
+        return file;
     }
     public FilesDto dtoFromFileEntity(Files files){
-        FilesDto filesDto = new FilesDto();
-        filesDto.setId(files.getId());
-        filesDto.setFileType(files.getFileType());
-        filesDto.setFile(files.getFile());
-        return filesDto;
+        FilesDto fileDto = new FilesDto();
+        fileDto.setId(files.getId());
+        fileDto.setFileType(files.getFileType());
+        fileDto.setFileName(files.getFileName());
+        return fileDto;
     }
 }
 
